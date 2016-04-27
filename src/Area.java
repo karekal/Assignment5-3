2cd407c1dc626421092f3321e1eaa8d7007117ec
srima karekal <srima589.karekal@gmail.com>
2016-04-27 10:57:04 +0530

    commit

---
+++ src/Area.java
@@ -0,0 +1,12 @@
+
+public class Area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  SquareRect sr=new SquareRect(3);
+  SquareRect sr2=new SquareRect(5,7);
+  System.out.println("area of the square:" + sr.area());
+  System.out.println("area of rectangle:"+ sr2.area());
+	}
+
+}
---
+++ src/SquareRect.java
@@ -0,0 +1,16 @@
+
+public class SquareRect {
+int length,breadth;
+  SquareRect(int l)
+  {
+	 length=breadth=l;
+ }
+ SquareRect(int l,int b)
+ {
+	length=l;
+	breadth=b;
+ }
+ int area(){
+	return length*breadth; 
+ }
+}
